44c22b86e71a79e98f22447a806faaa1d7857733
wallacermr <wallacermr@gmail.com>
2024-10-07 07:43:27 -0300

    first commit

---
+++ src/test/java/com/quote/authorization/MsAuthorizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quote.authorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAuthorizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
